728fd6efc0980b52747f354a47149d48078728cd
Michelangelo Capraro <michelangelo.capraro@autodesk.com>
2022-06-22 10:05:36 -0700

    updated to use a BASE_ENV env variable

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
-import axios from 'axios';
-import cookieSession from 'cookie-session';
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import axios from "axios";
+import cookieSession from "cookie-session";
 
 let app = express();
 
-app.use(cookieSession({
-  name: 'forge_session',
-  keys: ['forge_secure_key'],
-  maxAge: 60 * 60 * 1000 // 1 hour like the token
-}));
+app.use(
+  cookieSession({
+    name: "forge_session",
+    keys: ["forge_secure_key"],
+    maxAge: 60 * 60 * 1000 // 1 hour like the token
+  })
+);
 
 let clientId = process.env.FORGE_CLIENT_ID || "YOUR CLIENT ID";
 let clientSecret = process.env.FORGE_CLIENT_SECRET || "YOUR CLIENT SECRET";
 let serverPort = process.env.PORT || 3000;
-let callbackUrl = process.env.FORGE_CALLBACK_URL || `http://localhost:${serverPort}/callback/oauth`;
+let serverUrl = process.env.BASE_URL || "localhost";
+let callbackUrl =
+  process.env.FORGE_CALLBACK_URL || `${serverUrl}/callback/oauth`;
 
-app.get('/callback/oauth', async (req, res) => {
+app.get("/callback/oauth", async (req, res) => {
+  //console.log("/callback/token");
   const { code } = req.query;
 
   try {
     const response = await axios({
-      method: 'POST',
-      url: 'https://developer.api.autodesk.com/authentication/v1/gettoken',
+      method: "POST",
+      url: "https://developer.api.autodesk.com/authentication/v1/gettoken",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded"
       },
       data: `client_id=${clientId}&client_secret=${clientSecret}&grant_type=authorization_code&code=${code}&redirect_uri=${callbackUrl}`
-    })
+    });
 
     req.session = {
       access_token: response.data.access_token
     };
 
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
     console.log(error);
     res.end();
-  }    
+  }
 });
 
-app.get('/oauth/token', async (req, res) => {
+app.get("/oauth/token", async (req, res) => {
+  //console.log("/oauth/token");
+  console.log(req.session);
   if (!req.session?.access_token) {
     res.status(401).end();
     return;
@@ -50,18 +57,28 @@ app.get('/oauth/token', async (req, res) => {
   res.end(req.session.access_token);
 });
 
-app.get('/oauth/url', (req, res) => {
+app.get("/oauth/url", (req, res) => {
+  //console.log("/oauth/url");
   const url =
-      'https://developer.api.autodesk.com' +
-      '/authentication/v1/authorize?response_type=code' +
-      '&client_id=' + clientId +
-      '&redirect_uri=' + callbackUrl +
-      '&scope=data:read data:write data:create';
+    "https://developer.api.autodesk.com" +
+    "/authentication/v1/authorize?response_type=code" +
+    "&client_id=" +
+    clientId +
+    "&redirect_uri=" +
+    callbackUrl +
+    "&scope=data:read data:write data:create";
+
   res.end(url);
 });
 
-app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')));
+app.use(
+  express.static(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), "public")
+  )
+);
 
 app.listen(serverPort);
 
-console.log(`Open http://localhost:${serverPort} in a web browser in order to log in with your Autodesk account!`);
+console.log(
+  `Open ${serverUrl} in a web browser in order to log in with your Autodesk account!`
+);
